2f38aa4a7f2d0a0c253e23d21f893d93e2455dea
Aiden <33640860+SomeAspy@users.noreply.github.com>
2021-12-31 14:56:07 -0500

    IP reading setup

---
+++ script.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2021 Aiden Baker
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+(async () => {
+    let ip = await (await fetch(`https://ifconfig.me/ip`)).text();
+    console.log(ip);
+  })(); // Kinda ugly but that's the only thing you got if you don't have TLA
