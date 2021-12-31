081c20708e9e9d2ff8cd6cbbdfb93e7ab0cf4dba
Aiden <33640860+SomeAspy@users.noreply.github.com>
2021-12-31 15:36:01 -0500

    Clean up later

--- script.js
+++ script.js
@@ -6,4 +6,5 @@
 (async () => {
     let ip = await (await fetch(`https://ifconfig.me/ip`)).text();
     console.log(ip);
+    document.getElementById("ip").innerHTML = ip;
   })(); // Kinda ugly but that's the only thing you got if you don't have TLA
