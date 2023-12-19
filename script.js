16c076b4ae0830d40ede192d8f6ad3bdd0e4c92c
Aiden <amb@aspy.dev>
2023-12-19 17:02:42 -0500

    remove license headers

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// Copyright (c) 2021 Aiden Baker
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
-
 (async () => {
     let ip = await (await fetch(`https://ifconfig.me/ip`)).text();
     console.log(ip);
